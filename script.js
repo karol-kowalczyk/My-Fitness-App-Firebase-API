387987b03391c3296b7b7be5e950d706f42a48c1
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-20 23:22:11 +0200

    added a function to show the actual month in the main html side

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function init() {
     showDate();
+    showActualmonth();
 }
 
 function showDate() {
@@ -12,4 +13,15 @@ function showDate() {
     const dayOfWeek = daysOfWeek[today.getDay()];
     const fullDate = dayOfWeek + ', ' + day + '-' + month + '-' + year;
     date.innerText = fullDate;
+}
+
+function showActualMonth() {
+    let monthElement = document.getElementById('actualMonth');
+    const today = new Date();
+    const months = [
+        'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec',
+        'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'
+    ];
+    const monthName = months[today.getMonth()];
+    monthElement.innerText = monthName;
 }
